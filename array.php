d4ae9272bc9b560169032a1582d53b2ca8edddc2
edisonthk-mac <edisonthk@gmail.com>
2014-05-21 01:54:21 +0900

    foreachとarrayの説明を加え

---
+++ array.php
@@ -0,0 +1,12 @@
+<?php
+
+// arrayとは"配列"のこと
+// 配列の基礎的なコード
+
+$a = array("Apple","Mango","Orange","Watermelon");
+
+echo $a[0] . "<br>\n";  // Apple は表示します
+echo $a[1] . "<br>\n";  // Mango は表示します
+echo $a[2] . "<br>\n";  // Orange は表示します
+echo $a[3] . "<br>\n";  // Watermelon は表示します
+echo $a[4] . "<br>\n";  // エラー です
