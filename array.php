a245e29ec87eb46381c0a574c2cb52596fca5051
OYusuke <qdmy.75@gmail.com>
2014-05-27 00:10:17 +0900

    キーつきの配列を追加

--- array.php
+++ array.php
@@ -12,3 +12,17 @@ echo $a[3] . "<br>\n";  // Watermelon は表示します
 echo $a[4] . "<br>\n";  // エラー です
 
 echo "a";
+
+// $配列名 =  array("キー" => 値)
+
+ 	$result_list=array();//空の配列を宣言
+
+	$obj = array(
+			"title" => "タイトル"
+			"body"	=> "本文"
+		);
+	array_push($result_list, $obj);//$result_listに$objを追加
+	
+	foreach($result_list as $item){
+		echo "<li class='hit'><pre>{$item["body"]}</pre></li>";
+	}
